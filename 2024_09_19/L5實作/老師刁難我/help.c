112712b401e900f86a00827354a795560429d264
smcl <jjps104142@mail.jjps.kh.edu.tw>
2024-09-27 14:02:40 +0800

    V2.3

--- 2024_09_19/L5實作/老師刁難我/help.c
+++ 2024_09_19/L5實作/老師刁難我/help.c
@@ -10,41 +10,17 @@ int main()
 
     if (a<c)
     {
-        smcl=a;
-        a=c;
-        c=smcl;
+        smcl=a;a=c;c=smcl;
     }
     if (b<c)
     {
-        smcl=b;
-        b=c;
-        c=smcl;
+        smcl=b;b=c;c=smcl;
     }
     if (a<b)
     {
-        smcl=a;
-        a=b;
-        b=smcl;
+        smcl=a;a=b;b=smcl;
     }
     printf("%d\t%d\t%d",a,b,c);
-/*    if (a>b)
-    {
-        if (a>c)
-        {
-            if (b>c)
-            {
-                printf("%d\t%d\t%d",a,b,c);
-            }
-            else printf("%d\t%d\t%d",a,c,b);
-        }
-        else printf("%d\t%d\t%d",c,a,b);
-    }
-    else if (b>c)
-    {
-        if (a>c) printf("%d\t%d\t%d",b,a,c);
-        else printf("%d\t%d\t%d",b,c,a);
-    }
-    else printf("%d\t%d\t%d",c,b,a);
-*/
+
     return 0;
 }
